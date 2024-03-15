d44a86ab765fb8f4c271180f8ecfe56f89380e1d
overburn <overburn12@gmail.com>
2024-03-15 14:23:55 -0400

    update

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import database
 
 app = Flask(__name__)
 
+
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -19,13 +20,22 @@ def admin_required(f):
             return redirect(url_for('admin_login'))
     return decorated_function
 
+
+#--------------------------------------------------------------------------------------
+# environment variables / constants
+#--------------------------------------------------------------------------------------
+
+HOST='0.0.0.0'
+PORT=8082
+
 load_dotenv()
 app.secret_key = os.getenv('SECRET_KEY')
-admin_username = os.getenv('ADMIN_NAME')
-admin_password = os.getenv('ADMIN_PASSWORD')
-admin_password_hash = generate_password_hash(admin_password)  
-db_uri = os.getenv('DB_URI')
-database.init_db(db_uri)
+ADMIN_NAME = os.getenv('ADMIN_NAME')
+ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
+ADMIN_PASSWORD_HASH = generate_password_hash(ADMIN_PASSWORD)  
+DB_URI = os.getenv('DB_URI')
+
+database.init_db(DB_URI)
 
 #--------------------------------------------------------------------------------------
 # helper functions
@@ -34,10 +44,12 @@ database.init_db(db_uri)
 with open('static/favicon.ico', 'rb') as f:
     favicon_data = f.read()
 
+
 def set_admin_cookie():
     response = make_response("Cookie set")
     response.set_cookie('admin_cookie', 'true')
     return response
+
 
 #--------------------------------------------------------------------------------------
 # sql routes
@@ -71,6 +83,7 @@ def execute_sql_query():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+
 #--------------------------------------------------------------------------------------
 # admin routes
 #--------------------------------------------------------------------------------------
@@ -80,6 +93,7 @@ def execute_sql_query():
 def admin_sql():
     return render_template('admin_sql.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def admin_login():
     if 'logged_in' in session and session['logged_in']:
@@ -88,7 +102,7 @@ def admin_login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username == admin_username and check_password_hash(admin_password_hash, password):
+        if username == ADMIN_NAME and check_password_hash(ADMIN_PASSWORD_HASH, password):
             session['logged_in'] = True
             set_admin_cookie() 
             return redirect(url_for('admin_sql'))
@@ -96,10 +110,12 @@ def admin_login():
             flash('Invalid credentials')
     return render_template('admin_login.html')  # Your login page template
 
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     return redirect(url_for('admin_login'))
+
 
 #--------------------------------------------------------------------------------------
 
@@ -107,12 +123,14 @@ def logout():
 def favicon():
     return Response(favicon_data, mimetype='image/vnd.microsoft.icon')
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     path = request.path
     return f"404: The requested path '{path}' was not found.", 404
 
+
 #--------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8082)
+    app.run(host=HOST, port=PORT)
